0eb4e2d625d97781089b6674985d9b8ad4fae62b
Vrushabh <119349496+vrushgithub@users.noreply.github.com>
2024-03-05 11:39:46 +0000

    DataTypes

---
+++ Basics_01/ii_dataTypes.js
@@ -0,0 +1,25 @@
+// treat all JS code as newer vetrsion of JS
+"use strict"; 
+
+// alert (3 + 3) 
+// We are using nodeJS, not browser
+
+console.log("vrushabh");
+
+let name = "vrushabh Chaudhari"
+let age = 21
+let isLoggedIn = true
+
+// number => range 2 to power 53
+// bigint => number is very big then bigint also use
+// string => ""
+// boolean => true/false
+// null => stand alone value or Object
+// undefined => 
+// symbol => unique
+
+// Object
+
+console.log(typeof undefined);
+
+// ECMAscript site 
