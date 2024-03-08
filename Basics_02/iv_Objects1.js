82f4f1a01ae6e72064c7c7a1bbec9689c33d83b5
Vrushabh <119349496+vrushgithub@users.noreply.github.com>
2024-03-08 18:51:32 +0000

    JS OBJECTS

---
+++ Basics_02/iv_Objects1.js
@@ -0,0 +1,11 @@
+const course = {
+    coursename: "Hello JS User",
+    price: "999",
+    courseInstructor: "vrushabh"
+}
+// course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+const {price} = course
+console.log(price);
