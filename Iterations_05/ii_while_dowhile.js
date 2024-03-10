693efb086c5fb3ee5f0bde4a7525b1aa39071397
Vrushabh <119349496+vrushgithub@users.noreply.github.com>
2024-03-10 18:42:47 +0000

    do_while Loop

---
+++ Iterations_05/ii_while_dowhile.js
@@ -0,0 +1,27 @@
+// while Loop 
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['Flash' , 'Batman', 'Superman']
+let arr = 0
+while(arr < myArray.length){
+    console.log(`My ${arr} Superhero Name is ${myArray[arr]}`);
+    arr ++
+}
+
+// do_while Loop
+
+let score = 1
+do {
+    console.log(`Value is ${score}`);
+    score++
+} while (score <= 10);
+
+let scores = 11
+do {
+    console.log(`Value is ${scores}`);
+    scores++
+} while (scores <= 10);
